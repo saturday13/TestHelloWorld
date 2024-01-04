26c55abc2c63891c5d09b1d6bfd1f3509bf11fdd
LG <LG@DESKTOP-T5V7BKK>
2024-01-04 15:14:07 +0900

    초기 커밋.

---
+++ TestWin32Console/TestWin32Console.cpp
@@ -0,0 +1,13 @@
+// TestWin32Console.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!?" << std::endl;
+
+    return 0;
+}
+
